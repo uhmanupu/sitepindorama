ba75ae3bcda365ed687b6741afd80b2d02bcd485
uhmanupu <emanuelpu@gmail.com>
2025-12-26 21:19:23 -0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Se usar Vercel, pode apagar a linha 'base'. 
-  // Se usar GitHub Pages, mantenha como está abaixo:
-  base: '/sitepindorama/', 
-})
